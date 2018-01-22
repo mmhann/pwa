cc3014db5378da3f1e408a65fd8c77fa77ae872a
Michi <mikmeier@hotmail.com>
2018-01-22 15:50:34 +0100

    add

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,30 @@
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('first-app')
-      .then(function(cache) {
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/src/css/app.css',
-          '/src/js/app.js'
-        ])
-      })
-  );
-  return self.clients.claim();
+  // event.waitUntil(
+  //   caches.open('first-app')
+  //     .then(function(cache) {
+  //       cache.addAll([
+  //         '/',
+  //         '/index.html',
+  //         '/src/css/app.css',
+  //         '/src/js/app.js'
+  //       ])
+  //     })
+  // );
+    console.log('event1', event);
+
+  // return self.clients.claim();
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('event', event);
+    return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(res) {
-        return res;
-      })
-  );
+  // event.respondWith(
+  //   caches.match(event.request)
+  //     .then(function(res) {
+  //       return res;
+  //     })
+  // );
 });
